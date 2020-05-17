94387aa9f97008d053fc2e2554743e20670204b4
Celso Henrique <celso.henrique@hotmail.com.br>
2020-05-16 22:38:22 -0300

    Initial structure

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
